f095bc64d416c459585224de6e1cf558411963c6
zaleguo <zaleguo@tencent.com>
2020-06-12 18:05:07 +0800

    feat: 更新 rollup 配置

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,8 @@ import path from 'path';
 import babel from 'rollup-plugin-babel';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import pkg from './package.json';
+import { uglify } from 'rollup-plugin-uglify';
+import merge from 'lodash.merge';
 
 const extensions = ['.js', '.ts'];
 
@@ -9,20 +11,48 @@ const resolve = function (...args) {
     return path.resolve(__dirname, ...args);
 };
 
-module.exports = {
-    input: resolve('./src/index.ts'),
-    output: {
-        file: resolve('./', pkg.main), // 为了项目的统一性，这里读取 package.json 中的配置项
-        format: 'esm',
+// 打包任务的个性化配置
+const jobs = {
+    esm: {
+        output: {
+            format: 'esm',
+            file: resolve(pkg.module),
+        },
     },
-    plugins: [
-        nodeResolve({
-            extensions,
-            modulesOnly: true,
-        }),
-        babel({
-            exclude: 'node_modules/**',
-            extensions,
-        }),
-    ],
+    umd: {
+        output: {
+            format: 'umd',
+            file: resolve(pkg.main),
+            name: 'rem',
+        },
+    },
+    min: {
+        output: {
+            format: 'umd',
+            file: resolve(pkg.main.replace(/(.\w+)$/, '.min$1')),
+            name: 'rem',
+        },
+        plugins: [uglify()],
+    },
 };
+
+// 从环境变量获取打包特征
+const mergeConfig = jobs[process.env.FORMAT || 'esm'];
+
+module.exports = merge(
+    {
+        input: resolve('./src/index.ts'),
+        output: {},
+        plugins: [
+            nodeResolve({
+                extensions,
+                modulesOnly: true,
+            }),
+            babel({
+                exclude: 'node_modules/**',
+                extensions,
+            }),
+        ],
+    },
+    mergeConfig
+);
