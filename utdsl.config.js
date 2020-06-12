3d69fe754fceb44986e468dbc07f84704aa1966f
zaleguo <zaleguo@tencent.com>
2020-06-12 17:53:04 +0800

    feat: 代码格式优化

--- utdsl.config.js
+++ utdsl.config.js
@@ -1,11 +1,12 @@
 // utdsl 默认配置
 module.exports = {
-    fireevent: {
-    },
-    module: [{
-        async: '',
-        test: "*",
-        use: {},
-        ui: ''
-    }]
-}
+    fireevent: {},
+    module: [
+        {
+            async: '',
+            test: '*',
+            use: {},
+            ui: '',
+        },
+    ],
+};
