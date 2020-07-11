b96a5504d436f2cdf30bd518bd47b671a33a32a2
zaleguo <zaleguo@tencent.com>
2020-07-11 16:14:36 +0800

    feat: 代码优化

--- src/parser/grammar.ts
+++ src/parser/grammar.ts
@@ -314,6 +314,101 @@ export const parserThis = (thisContent: string): IOperationDesc[] => {
 };
 
 /**
+ * 解析表达式成操作描述对象
+ * @param expression 表达式
+ */
+export const parserExpressionOperationDesc = (expression = ''): IExpressOperationDesc | Error => {
+    // 获取表达式的值
+    expression = expression.trim();
+    const firstOperationIndex = expression.indexOf(':');
+    const expressionValue = expression.slice(0, firstOperationIndex);
+
+    // 不存在操作符，整个表达式即为值 (值只能是纯文本)
+    if (firstOperationIndex === -1) {
+        return {
+            value: expression,
+        };
+    }
+
+    // 获取表达式中可能的操作索引值
+    const matchOperationList: IMatchOperationListItem[] = [];
+    const re = new RegExp(`:(${EXPRESSION_OPERATION_NAME_LIST.join('|')}):`, 'g');
+    let match = null;
+    while ((match = re.exec(expression)) != null) {
+        matchOperationList.push({
+            index: match.index,
+            value: match[0],
+        });
+    }
+
+    // 如果操作列表为0，则表示填写的没有被支持
+    if (matchOperationList.length === 0) {
+        return new Error(`表达式 "${expression}" 中的操作符都不被支持.`);
+    }
+
+    // 检查下表达式的内容是否重复，如果有相同的 key, 则报错，存在语法错误
+    if (isRepeat(matchOperationList.map((item) => item.value))) {
+        return new Error(`表达式 "${expression}" 中存在多个相同的操作符.`);
+    }
+
+    let result: IExpressOperationDesc = {
+        value: '',
+    };
+
+    // 只有一个操作符的情况
+    if (matchOperationList.length === 1) {
+        const [value, operationValue] = expression.split(matchOperationList[0].value);
+        result = {
+            value,
+            [matchOperationList[0].value]: operationValue,
+        };
+    } else {
+        result.value = expressionValue;
+        // 解析操作符
+        matchOperationList.forEach((item, index) => {
+            // 获取操作的值的索引位置
+            const startIndex = item.index + item.value.length;
+            const endIndex = matchOperationList[index + 1] ? matchOperationList[index + 1].index : expression.length;
+
+            const operationValue = expression.slice(startIndex, endIndex);
+            result[item.value] = operationValue;
+        });
+    }
+
+    // 如果存在 expression 操作符号，要检查下 expression 对应的值是否是 js 表达式
+    const expressionOperationValue = result[`:${EXPRESSION_OPERATION_MAP.EXPRESSION}:`];
+    if (expressionOperationValue) {
+        if (!isVariableExpressionsString(expressionOperationValue)) {
+            return new Error(`expression 参数的值 "${expressionOperationValue}" 不是一个可执行的 JS 表达式.`);
+        }
+    }
+
+    return result;
+};
+
+/**
+ * 将表达式作为数据导入参数
+ * @param expression 表达式
+ */
+export const paraseDataParamsExpression = (expression = ''): IDataParamsDesc => {
+    // 解析操作描述
+    const operationDesc = parserExpressionOperationDesc(expression);
+
+    if (operationDesc instanceof Error || _.isUndefined(operationDesc[KEY_WORDS.FROM])) {
+        return {
+            isValidate: false,
+            message: `表达式 "${expression}" 解析错误或不存在数据从外部导入的语法`,
+        };
+    }
+    return {
+        isValidate: true,
+        path: operationDesc[KEY_WORDS.FROM],
+        variableName: operationDesc.value,
+        asName: operationDesc[KEY_WORDS.AS],
+    };
+};
+
+/**
  * 解析主函数
  * @param params
  */
@@ -420,99 +515,4 @@ export const parse = (params: IParse): void => {
     });
 
     return;
-};
-
-/**
- * 解析表达式成操作描述对象
- * @param expression 表达式
- */
-export const parserExpressionOperationDesc = (expression = ''): IExpressOperationDesc | Error => {
-    // 获取表达式的值
-    expression = expression.trim();
-    const firstOperationIndex = expression.indexOf(':');
-    const expressionValue = expression.slice(0, firstOperationIndex);
-
-    // 不存在操作符，整个表达式即为值 (值只能是纯文本)
-    if (firstOperationIndex === -1) {
-        return {
-            value: expression,
-        };
-    }
-
-    // 获取表达式中可能的操作索引值
-    const matchOperationList: IMatchOperationListItem[] = [];
-    const re = new RegExp(`:(${EXPRESSION_OPERATION_NAME_LIST.join('|')}):`, 'g');
-    let match = null;
-    while ((match = re.exec(expression)) != null) {
-        matchOperationList.push({
-            index: match.index,
-            value: match[0],
-        });
-    }
-
-    // 如果操作列表为0，则表示填写的没有被支持
-    if (matchOperationList.length === 0) {
-        return new Error(`表达式 "${expression}" 中的操作符都不被支持.`);
-    }
-
-    // 检查下表达式的内容是否重复，如果有相同的 key, 则报错，存在语法错误
-    if (isRepeat(matchOperationList.map((item) => item.value))) {
-        return new Error(`表达式 "${expression}" 中存在多个相同的操作符.`);
-    }
-
-    let result: IExpressOperationDesc = {
-        value: '',
-    };
-
-    // 只有一个操作符的情况
-    if (matchOperationList.length === 1) {
-        const [value, operationValue] = expression.split(matchOperationList[0].value);
-        result = {
-            value,
-            [matchOperationList[0].value]: operationValue,
-        };
-    } else {
-        result.value = expressionValue;
-        // 解析操作符
-        matchOperationList.forEach((item, index) => {
-            // 获取操作的值的索引位置
-            const startIndex = item.index + item.value.length;
-            const endIndex = matchOperationList[index + 1] ? matchOperationList[index + 1].index : expression.length;
-
-            const operationValue = expression.slice(startIndex, endIndex);
-            result[item.value] = operationValue;
-        });
-    }
-
-    // 如果存在 expression 操作符号，要检查下 expression 对应的值是否是 js 表达式
-    const expressionOperationValue = result[`:${EXPRESSION_OPERATION_MAP.EXPRESSION}:`];
-    if (expressionOperationValue) {
-        if (!isVariableExpressionsString(expressionOperationValue)) {
-            return new Error(`expression 参数的值 "${expressionOperationValue}" 不是一个可执行的 JS 表达式.`);
-        }
-    }
-
-    return result;
-};
-
-/**
- * 将表达式作为数据导入参数
- * @param expression 表达式
- */
-export const paraseDataParamsExpression = (expression = ''): IDataParamsDesc => {
-    // 解析操作描述
-    const operationDesc = parserExpressionOperationDesc(expression);
-
-    if (operationDesc instanceof Error || _.isUndefined(operationDesc[KEY_WORDS.FROM])) {
-        return {
-            isValidate: false,
-            message: `表达式 "${expression}" 解析错误或不存在数据从外部导入的语法`,
-        };
-    }
-    return {
-        isValidate: true,
-        path: operationDesc[KEY_WORDS.FROM],
-        variableName: operationDesc.value,
-        asName: operationDesc[KEY_WORDS.AS],
-    };
 };
